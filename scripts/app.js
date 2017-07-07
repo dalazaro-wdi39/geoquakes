01e6c2ddf00888de2b33db22e2bbc99a526d5007
dalazaro <daryl.lazaro@gmail.com>
2017-07-07 16:40:17 -0700

    Created working appends, map, and home marker

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,42 @@
 // define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var quakesLink = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson';
 
-$(document).ready(function() {
+console.log('sanity check');
+
+
+$(document).ready(function () {
   console.log("Let's get coding!");
-  // CODE IN HERE!
+
+  $.ajax({
+    method: 'GET',
+    url: quakesLink,
+    dataType: 'json',
+    success: onSuccess
+  });
+
+  function onSuccess (dataResponse) {
+    for (i = 0; i <= 25; i++) {
+      $('.earthquakeData').append(`<li>${dataResponse.features[i].properties.title}</li>`)
+    }
+  }
 
 });
+
+// Google Map
+$('#map').append(`initMap`);
+
+function initMap () {
+  var myLatLng = {lat: 37.791, lng: -122.401};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 2
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: 37.791, lng: -122.401},
+    map: map,
+    title: 'Home'
+  });
+
+};
